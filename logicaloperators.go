2fe6490f5700a3340f9488a91801addae829b68a
golang_user <smutalova45@gmail.com>
2023-11-18 11:37:18 +0600

    added

---
+++ logicaloperators.go
@@ -0,0 +1,161 @@
+package main
+
+import "fmt"
+
+func main(){
+  //                            PRACTISE 
+  // var (
+  //   a int = 2
+  //   b int = 6
+  // )
+  // fmt.Printf("a %d b %d ", b-a+a, a-b+b )
+
+  // fmt.Printf("a = %d b= %d", a, b)
+  // fmt.Printf("a= %d b= %d", b, a)
+  
+
+  // c := 34
+  // result := c % 10
+  // result2 := c / 10 % 10
+  // fmt.Printf(" onliklar %d birliklar %d", result2, result)
+
+  // d := 12
+  // sum := d % 10
+  // sum1 := d / 10 % 10
+  // sum3 := sum + sum1
+
+  // fmt.Printf(" sum %d", sum3)
+
+  // fmt.Printf("teskarisi %d%d", sum,sum1)
+
+  // uchxonalison:=123
+  // javob:=uchxonalison/10 //23 ni berad
+  // javob1:=javob / 10 //1 ni beradi
+  // javob2:=javob%10 // 3 ni berad
+  // javob3:=javob/10%10 //2 ni berad
+  // result:=javob2*100+javob1*10+javob3 //312
+  // result1:= javob3*100+javob3*10+javob1 //231
+  // result2:=javob3*100+javob1*10+javob2 //213
+  
+  // 7// fmt.Printf("javob %d ", javob1 )
+    //  8 fmt.Printf("result //8 %d " , javob1+javob2+javob3)
+  // 9 fmt.Printf("result of 9 %d ", javob2,javob3,javob1)
+  //10 fmt.Printf("result 10 %d ," result)
+  //11 fmt.Println(result1) 
+  //12 fmt.Println(result2)
+
+  
+  //homework(logical operators)
+  // uchxonalison:=123
+  // result1:=uchxonalison/10 //23
+  // result2:=result1/10 // 1
+  // result3:=result1%10 //3
+  // result4:=result1/10%10 //2
+  // result5:=result2*100+result3*10+result4
+  // fmt.Println("result = ", result5)
+  
+  
+  // var son =1210 //2 ni chiqarish kere
+  // result:=son%1000 //210
+  // result1:=result/10/10%10 //2
+  // fmt.Println(result1)
+
+  
+  //result2:=result/10/10
+  //fmt.Println(result2)
+
+  //5
+  // var sekund int
+  // fmt.Scan(&sekund)
+  // soat:=sekund/3600
+  // // fmt.Println(soat)
+
+  // minut:= sekund/60
+  // // fmt.Println("minut " , minut)
+
+  
+  // fmt.Printf("minut : %d , sekund: %d ", minut, sekund)
+
+  // fmt.Printf("soat %d sekund: %d ", soat, sekund)
+  
+  // fmt.Printf("soat : %d , minut : %d , sekund: %d ", soat, minut,sekund)
+
+            // HOMEWORK(praktika)
+  //1
+  var tortburchaktomoni=45 //(sm) togri tortburchak
+  var tomon2chi=21
+  yuzi:=tortburchaktomoni*tomon2chi
+  fmt.Println("yuzi",yuzi)
+  //2
+  var son int
+  fmt.Scan(&son)
+  kub:=son*son*son
+  fmt.Println("kubi: ",kub)
+
+  //3
+  radius:=34.5
+  yuza:=3.14*radius*radius
+  fmt.Println("doira yuzasi: ",yuza)
+  //4
+  perimeter:=tortburchaktomoni+tomon2chi*2
+  //5
+  //sfera
+  radiussfera:=12.6
+  v:=4/3*3.14*radiussfera*radiussfera*radiussfera
+  yuzasfera:=4*3.14*radiussfera*radiussfera
+  fmt.Printf("yuza %d hajm %d ", yuzasfera, v)
+
+
+
+
+
+  // praktika 4-(dars)
+
+  // uchburchak 
+  // var(
+  //   a =2
+  //   b=3
+  //   c=7
+
+  // )
+  //  var check bool
+  // check=a+b>=c 
+  // fmt.Println(check)
+
+  //kvadrat
+  
+  // var a=3
+  // yuzi:=a*a
+  // fmt.Println("yuzi ",yuzi)
+
+  // aylana
+  // var diametr =3.5
+  // fmt.Println("uzunligi ", diametr*3.14)
+
+  //dastur
+  // var a int 
+  // fmt.Scan(&a)
+  // fmt.Println(a*a)
+
+  // tortburchak
+  //  var(
+  //   c int
+  //   d int
+  //   f int
+  //   g int
+  //  )
+  //  fmt.Scan(&c)
+  //  fmt.Scan(&d)
+  //  fmt.Scan(&f)
+  //  fmt.Scan(&g)
+  //  fmt.Printf("perimeter %d", c+d+f+g)
+
+   //kvadrat
+  //  var h=5
+  //  fmt.Println(" perimetr", h*4)
+
+
+    
+
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
 import "fmt"
 func main(){
-	fmt.Println("All homeworks")
+	// fmt.Println("All homeworks")
 }
